d2c28aec269dedf28f85bc4152d3067613ce7e04
gabicilfone <cilfonegabriel@gmail.com>
2024-01-05 03:35:59 -0300

    Fix bug

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -11,23 +11,24 @@ document.addEventListener("DOMContentLoaded", function() {
         }   
 
     },300));
+
+    function sendSearchQuery(query) {
+        fetch('/search', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+          },
+          body: JSON.stringify({ query: query })
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+      }
 });
 
-function sendSearchQuery(query) {
-    fetch('/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-      },
-      body: JSON.stringify({ query: query })
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-  }
