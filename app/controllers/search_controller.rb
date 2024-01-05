433913b29908c7173025cd749db0dfa6d939a43f
gabicilfone <cilfonegabriel@gmail.com>
2024-01-05 00:48:59 -0300

    Add method create

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,15 @@
 class SearchController < ApplicationController
 
   protect_from_forgery with: :null_session
-  
+
   def create
+    @search = Search.new(query: params[:query], ip_address: request.remote_ip)
+
+    if @search_query.save
+      render json: { status: 'success', message: 'Search query recorded successfully' }
+    else
+      render json: { status: 'error', message: 'Failed to record search query' }
+    end
   end
 
   def index
